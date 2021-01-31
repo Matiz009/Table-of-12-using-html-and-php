2c587009ee18098685bb627a15d141b34d5792ad
MATI UL REHMAN <74356765+Matiz009@users.noreply.github.com>
2021-01-31 16:38:03 +0500

    table of 12

---
+++ alpha.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<body style="color: red;background-color: black">
+<h1 style="color: aquamarine">TABLE OF TWELVE</h1>
+</body>
+</html>
+
+<?php
+$m=12;
+$x=1;
+$a=12;
+while($m<=120&&$x<=10){
+    echo $a.""."*".$x.""."=".$m."<h1></h1>";
+    $x++;
+    $m=$m+12;
+}
+?>
